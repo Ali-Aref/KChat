a95244e0e387b5178a9c5e48a3202dfc91fe4bb2
Ali Aref <aliaref.js@gmail.com>
2024-11-03 15:12:38 +0430

    add(methods): patch and delete

--- server/src/app/api/todo/[id]/route.ts
+++ server/src/app/api/todo/[id]/route.ts
@@ -1,17 +1,38 @@
 import { db } from "@/db";
 import { todo } from "@/db/schema/todo";
 import { eq } from "drizzle-orm";
-import { NextRequest } from "next/server";
+import { NextRequest, NextApiRequest } from "next/server";
 
 type PageParams = {
-  id: string;
+	id: string;
 };
 
-export async function GET( request: NextRequest, { params }: { params: PageParams }) {
-  // const searchParams = request.nextUrl.searchParams;
+export async function GET(request: NextRequest, { params }: { params: PageParams }) {
+	// const searchParams = request.nextUrl.searchParams;
 	const { id } = await params;
 
 	const item = await db.select().from(todo).where(eq(todo.id, parseInt(id)))
 
-  return Response.json(item);
+	return Response.json(item);
 }
+
+export async function PATCH(request: NextApiRequest, { params }: { params: PageParams }) {
+	const data = await request.json();
+	const { id } = await params;
+
+	await db.update(todo).set(data).where(eq(todo.id, parseInt(id)))
+	const item = await db.select().from(todo).where(eq(todo.id, parseInt(id)))
+
+	return Response.json(item);
+}
+
+export async function DELETE(request: NextApiRequest, { params }: { params: PageParams }) {
+	const { id } = await params;
+
+	await db.delete(todo).where(eq(todo.id, parseInt(id)))
+
+	return new Response(null, {
+		status: 204
+	});
+}
+
