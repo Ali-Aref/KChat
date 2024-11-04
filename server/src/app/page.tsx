616235d66071445893ccf673433e52ac593cdf05
Ali Aref <aliaref.js@gmail.com>
2024-11-04 09:53:59 +0430

    add(package): shadcn ui

--- server/src/app/page.tsx
+++ server/src/app/page.tsx
@@ -1,13 +1,15 @@
 import { db } from "@/db";
 import { sql } from "drizzle-orm";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
 	const todos = await db.execute(sql`select * from todos`)
 	console.log("todo: ", todos)
 
   return (
-    <div className="flex flex-1 h-screen items-center justify-center">
-			<h1>Hola</h1>
+    <div className="flex bg-black flex-col flex-1 gap-3 h-screen items-center justify-center">
+			<h1 className="text-white">Hola</h1>
+			<Button className="bg-white text-black">Add</Button>
     </div>
   );
 }
