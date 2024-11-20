f9409724795980b0805696f2bc83abe96fd73395
Ali Aref <aliaref.js@gmail.com>
2024-11-20 06:43:12 +0430

    update(profile-crud): finished

--- django/user/api/serializers.py
+++ django/user/api/serializers.py
@@ -10,25 +10,51 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = USER
-        fields = "__all__"
-        extra_kwargs = {
-            "password": {"write_only": True},
-        }
+        exclude = ("password", "last_login")
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = USER
+        fields = "first_name", "last_name", "username", "email"
 
 
 class ProfileListSerializer(serializers.ModelSerializer):
-    # START: from here
-    # make the crud for profile and user
     user = UserSerializer(
         fields=("username", "first_name", "last_name"),
     )
 
     class Meta:
         model = Profile
+        fields = "id", "gender", "avatar", "user"
+
+
+class ProfileDetailsSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Profile
+        fields = "id", "gender", "avatar", "user"
+
+
+class ProfileUpdateSerializer(serializers.ModelSerializer):
+    user = UserSerializer(
+        fields=("username", "first_name", "last_name", "email"),
+    )
+
+    class Meta:
+        model = Profile
         fields = "__all__"
-        extra_kwargs = {
-            "user": {"read_only": True},
-            "bio": {"write_only": True},
-            "cover_photo": {"write_only": True},
-            "birth_date": {"write_only": True}
-        }
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user", None)
+        if user_data:
+            user_serializer = UserUpdateSerializer(
+                data=user_data,
+                instance=instance.user,
+                partial=True,
+            )
+            is_valid_user = user_serializer.is_valid(raise_exception=True)
+            if is_valid_user:
+                user_serializer.save()
+        return super().update(instance, validated_data)
--- django/user/api/views.py
+++ django/user/api/views.py
@@ -8,9 +8,12 @@ from . import permissions, serializers
 class ProfileModelViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsProfileOwner]
     map_serializer_classes = {
-        "list": serializers.ProfileListSerializer
+        "list": serializers.ProfileListSerializer,
+        "retrieve": serializers.ProfileDetailsSerializer,
+        "partial_update": serializers.ProfileUpdateSerializer,
     }
     queryset = Profile.objects.filter()
+    http_method_names = ['get', 'patch', 'head']
 
     def get_serializer_class(self):
         return self.map_serializer_classes.get(self.action)
