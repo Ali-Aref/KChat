5324865a8b53e86b4d0badd43cbb1e54232cc383
Ali Aref <aliaref.js@gmail.com>
2024-11-25 21:37:45 +0430

    update(cleanup): project

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,13 +1,13 @@
+import React, {useEffect} from 'react'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 import '../global.css'
+import { useColorScheme } from 'react-native';
 
-import { useColorScheme } from '@/components/useColorScheme';
 
 if (__DEV__) {
   require("../ReactotronConfig");
@@ -56,8 +56,7 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '@/components/ui/Text';
+
+export default function Index() {
+  return (
+		<View className='flex flex-1 items-center justify-center'>
+			<Text>Hello world!</Text>
+		</View>
+  );
+}
+
