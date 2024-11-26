478a57ad83fac6b833368ed142c779432d3e10ca
Ali Aref <aliaref.js@gmail.com>
2024-11-26 08:47:43 +0430

    add(file): colors

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -5,13 +5,17 @@ import Text from '@/components/ui/Text';
 export default function Index() {
   return (
 		<View className='flex flex-1 items-center justify-center'>
-			<Pressable className='p-10 rounded bg-red-800
-			hover:bg-green-800
+			<Pressable className='p-10 rounded bg-blue-700
 			active:bg-blue-800
+			active:ring-4
+			active:ring-black
 			focus:bg-yellow-800
 			group
+			gap-10
+			first-child:text-blue-400
 			'>
-				<Text className={"group-active:text-white"}>Hello World</Text>
+				<Text size='lg' className={"text-white group-active:font-bold"}>Shahy Payment</Text>
+				<Text size='lg' className={"text-white"}>AHD Payment</Text>
 			</Pressable>
 		</View>
   );
