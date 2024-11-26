6b33763882357fdae8e0101b645109df92d908e9
Ali Aref <aliaref.js@gmail.com>
2024-11-26 07:09:50 +0430

    update(index)

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 import Text from '@/components/ui/Text';
 
 export default function Index() {
   return (
 		<View className='flex flex-1 items-center justify-center'>
-			<Text>Hello world!</Text>
+			<Pressable className='p-10 rounded bg-red-800
+			hover:bg-green-800
+			active:bg-blue-800
+			focus:bg-yellow-800
+			group
+			'>
+				<Text className={"group-active:text-white"}>Hello World</Text>
+			</Pressable>
 		</View>
   );
 }
