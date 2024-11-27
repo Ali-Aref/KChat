ce269e91929e188d34ea1088f35542495beb7cf7
Ali Aref <aliaref.js@gmail.com>
2024-11-27 11:07:50 +0430

    temp-update

--- temp/redux/index.js
+++ temp/redux/index.js
@@ -1,27 +1,55 @@
-// type of the action
-const CAKE_OREDERED = "CAKE_OREDERED";
-
-// action
-function orderCake() {
-  return {
-    type: CAKE_OREDERED,
-    count: 1,
-  };
-}
+const redux = require("redux")
 
 const initialState = {
-  numberOfCakes: 10,
-};
+	numberOfCakes: 10,
+}
 
-const reducer = (perviousState = initialState, action) => {
-  // returns new state
-  switch (action.type) {
-    case CAKE_OREDERED:
-      return {
-        ...perviousState,
-        numberOfCakes: perviousState.numberOfCakes - action.count,
-      };
-    default:
-      return perviousState;
-  }
-};
+const CAKE_ORDER = "CAKE_ORDER"
+const ICE_ORDER = "ICE_ORDER"
+
+function orderCake(payload=-1) {
+	return {
+		type: CAKE_ORDER,
+		payload,
+	}
+}
+function orderIce(payload=1){
+	return {
+		type: ICE_ORDER,
+		payload,
+	}
+}
+
+function reducer(state = initialState, action){
+	switch(action.type){
+		case CAKE_ORDER:
+			return {
+				...state,
+				numberOfCakes: state.numberOfCakes + action.payload
+			}
+		default:
+			return state
+	}
+}
+
+// store
+// hold states
+const createStore = redux.createStore
+const store = createStore(reducer)
+
+// get states
+console.log(store.getState())
+
+// listener
+const unsubscribe = store.subscribe(()=>{
+	console.log("listener: ", store.getState())
+})
+
+// dispatch
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake(10))
+
+// unsubscribe
+unsubscribe()
