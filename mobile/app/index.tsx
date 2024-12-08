2199bc627e55eb51f786eb52455515b528d91759
Ali Aref <aliaref.js@gmail.com>
2024-12-08 08:37:07 +0430

    init(new-expo-project)

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,19 +1,6 @@
-import { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
-  const [todo, setTodo] = useState<any>(null);
-
-  const fetchTodo = () => {
-		setTodo(null)
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then((json) => {
-				//console.log(json)
-				setTodo(json)
-			});
-  };
-
   return (
     <View
       style={{
@@ -23,8 +10,6 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-			<Button title="Fetch Todo" onPress={fetchTodo} />
-      <Text>{JSON.stringify(todo, null, 2)}</Text>
     </View>
   );
 }
