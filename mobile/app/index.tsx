3ed36b989627fd96de64bce759e0a5108162eac3
Ali Aref <aliaref.js@gmail.com>
2024-12-08 12:10:08 +0430

    init(new-proj)

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,19 @@
 import { Text, View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "dodgerblue",
+  },
+});
