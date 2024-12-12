14b7746a36a4c84e7a7e9b74fbc164f15bcb6da1
Ali Aref <aliaref.js@gmail.com>
2024-12-12 10:02:54 +0430

    add(clean-expo-project)

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,19 +1,15 @@
 import { Text, View } from "react-native";
-import { StyleSheet } from "react-native-unistyles";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "dodgerblue",
-  },
-});
