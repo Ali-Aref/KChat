745e97c625ea1a348e2a85df22274baf9fc7ee53
Ali Aref <aliaref.dev@gmail.com>
2024-12-17 13:39:45 +0430

    update(statusbar & navigationbar): based on device colorscheme

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,8 +1,21 @@
+import { darkTheme, lightTheme } from "@/theme/themes";
 import { Stack } from "expo-router";
-import { UniStack } from "@/theme/navigation";
-
+import { StatusBar, useColorScheme } from "react-native";
+import * as NavigationBar from "expo-navigation-bar"
 
 export default function RootLayout() {
+	const colorScheme = useColorScheme()
+
+	if (colorScheme === "dark"){
+		StatusBar.setBarStyle("light-content")
+		StatusBar.setBackgroundColor(darkTheme.colors.background)
+		NavigationBar.setBackgroundColorAsync(darkTheme.colors.background)
+	} else {
+		StatusBar.setBarStyle("dark-content")
+		StatusBar.setBackgroundColor(lightTheme.colors.background)
+		NavigationBar.setBackgroundColorAsync(lightTheme.colors.background)
+	}
+
   return (
     <Stack screenOptions={{
 			headerShown: false,
